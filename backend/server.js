48d4be47ebacb3464759ed714f7e22daa07c8169
RNS-49 <133084312+RNS-49@users.noreply.github.com>
2025-02-18 13:26:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -19,7 +18,7 @@ app.use(cors({
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+
 
 
 
@@ -37,16 +36,6 @@ app.use("/api/users",userRoutes);    // middleware setup for users list
 
 app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
-});
-
-app.get("/",(req,res) => {
-  res.send("hello world");
-})
-
-
-                    
 
 app.listen(5000,()=>{ 
   connectToMongoDB();                              // connecting to database when the server starts
