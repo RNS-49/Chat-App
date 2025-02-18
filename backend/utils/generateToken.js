90a13e52311b2735f85234b417aff8a331a4070a
RNS-49 <133084312+RNS-49@users.noreply.github.com>
2025-02-18 13:07:02 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateTokenAndSetCookie = (userId,res)=>{
   res.cookie("jwt",token,{
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly:true,
-    sameSite: process.env.NODE_ENV === "development" ? "lax": "strict",
-    secure:process.env.NODE_ENV !== "development"
+    sameSite: process.env.NODE_ENV === "production" ? "none": "lax",
+    secure:process.env.NODE_ENV === "production",
     });
 };
 
